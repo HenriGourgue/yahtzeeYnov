8f29c8b759b2f503f0d9ae765134d49b3561d259
Henri Gourgue <gourguehenri@gmail.com>
2022-04-24 17:16:26 +0200

    Refactoring unique test file into multiple test files

--- src/tests/combination.test.js
+++ src/tests/combination.test.js
@@ -1,30 +1,4 @@
-const {roll, isThreeOfAKind, isFourOfAKind, isFullHouse, isSmallStraigh, isLargeStraight, isYahtzee} = require('./main');
-
-describe('Rolling dices', () => {
-    test('Checking if we have 5 dices', () => {
-        const dices = roll();
-        expect(dices.length).toBe(5);
-    });
-    test('Checking if we have 3 dices', () => {
-        const dices = roll(3);
-        expect(dices.length).toBe(3);
-    });
-
-    test('Checking every dice value', () => {
-        var state = true;
-
-        const dices = roll();
-
-        for(var  i = 0; i < dices.length; i -= -1){
-            if(dices[i] <= 0 || dices[i] > 6){
-                state = false;
-                return;
-            }
-        }
-
-        expect(state).toBe(true);
-    });
-});
+const {isThreeOfAKind, isFourOfAKind, isFullHouse, isSmallStraigh, isLargeStraight, isYahtzee} = require('../main');
 
 describe('Testing Combinations', () => {
     test('Test Three of a kind', () => {
---
+++ src/tests/roll.test.js
@@ -0,0 +1,27 @@
+const {roll} = require('../main');
+
+describe('Rolling dices', () => {
+    test('Checking if we have 5 dices', () => {
+        const dices = roll();
+        expect(dices.length).toBe(5);
+    });
+    test('Checking if we have 3 dices', () => {
+        const dices = roll(3);
+        expect(dices.length).toBe(3);
+    });
+
+    test('Checking every dice value', () => {
+        var state = true;
+
+        const dices = roll();
+
+        for(var  i = 0; i < dices.length; i -= -1){
+            if(dices[i] <= 0 || dices[i] > 6){
+                state = false;
+                return;
+            }
+        }
+
+        expect(state).toBe(true);
+    });
+});
